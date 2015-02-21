6c78ca40a4eca91c6aeff8e0bcdf172de12e2a33
Alberto Arena <arena.alberto@gmail.com>
2015-02-21 12:36:17 +0000

    Implement calculator with shunting-yard algorithm

--- src/Calculator/Calculator.php
+++ src/Calculator/Calculator.php
@@ -2,8 +2,17 @@
 namespace Calculator;
 
 
+use Calculator\Number\Number;
+use Calculator\Operator\Operator;
 use Calculator\Stack\Stack;
 
+/**
+ * Class Calculator
+ * @package Calculator
+ *
+ * The logic is inspired by shunting-yard algorithm
+ * http://en.wikipedia.org/wiki/Shunting-yard_algorithm
+ */
 class Calculator
 {
     /** @var \Calculator\Stack\Stack */
@@ -18,25 +27,89 @@ class Calculator
         $this->operators = new Stack();
     }
 
+    /**
+     * @param $item
+     */
     protected function process($item)
     {
-        // @todo
+        switch (true)
+        {
+            case $item instanceof Operator:
+                // Get the latest operator in stack
+                $lastInStack = $this->operators->current();
+
+                if (!is_null($lastInStack)) {
+
+                    // Check precedence
+                    if ($item->getPrecedence() > $lastInStack->getPrecedence()) {
+                        // Push current operator
+                        $this->operators->push($item);
+                    }
+                    else {
+                        // Process latest operator in stack
+                        $value2 = $this->output->pop()->getValue();
+                        $value1 = $this->output->pop()->getValue();
+                        $this->output->push(new Number($lastInStack->execute($value1, $value2)));
+                        // Pop last operator
+                        $this->operators->pop();
+                        // Push current operator
+                        $this->operators->push($item);
+                    }
+                }
+                else {
+                    // Empty stack, push current operator
+                    $this->operators->push($item);
+                }
+                break;
+
+            case $item instanceof Number:
+                $this->output->push($item);
+                break;
+
+            default:
+                // Never occurs
+                break;
+        }
     }
 
+    protected function finaliseProcess()
+    {
+        // Reduce operators in stack
+        while ($this->operators->count()) {
+            $value2 = $this->output->pop()->getValue();
+            $value1 = $this->output->pop()->getValue();
+            $this->output->push(new Number($this->operators->pop()->execute($value1, $value2)));
+        }
+    }
+
+    /**
+     * @param string $operator
+     * @return $this
+     */
     public function operator($operator)
     {
-        $this->process($operator);
+        $this->process(OperatorFactory::createOperator($operator));
         return $this;
     }
 
+    /**
+     * @param int $number
+     * @return $this
+     */
     public function number($number)
     {
-        $this->process($number);
+        $this->process(new Number($number));
         return $this;
     }
 
     public function execute()
     {
-        return 0;
+        $this->finaliseProcess();
+
+        if ($this->output->count()) {
+            return $this->output->pop()->getValue();
+        } else {
+            return 0;
+        }
     }
 }
--- tests/Calculator/CalculatorTest.php
+++ tests/Calculator/CalculatorTest.php
@@ -14,13 +14,40 @@ class CalculatorTest extends \PHPUnit_Framework_TestCase
         $this->calculator = new Calculator();
     }
 
-    // 1 + 1 * 3 + 3 = 7
-    public function testCalculate1()
+    // 1 + 1 * 3 + 3 ==> 7
+    public function testCalculateInt1()
     {
         $this->calculator->number(1)
             ->operator('+')->number(1)
             ->operator('*')->number(3)
             ->operator('+')->number(3);
         $this->assertEquals(7, $this->calculator->execute());
+    }
+
+    // 5 + 8 / 4 * 3 - 1 ==> 10
+    public function testCalculatorInt2()
+    {
+        $this->calculator->number(5)
+            ->operator('+')->number(8)
+            ->operator('/')->number(4)
+            ->operator('*')->number(3)
+            ->operator('-')->number(1);
+        $this->assertEquals(10, $this->calculator->execute());
+    }
+
+    // 0.5 + 30 / 8 + 3.6 * 1.4 ==> 9.29
+    public function testCalculatorFloat1()
+    {
+        $this->calculator->number(.5)
+            ->operator('+')->number(30)
+            ->operator('/')->number(8)
+            ->operator('+')->number(3.6)
+            ->operator('*')->number(1.4);
+        $this->assertEquals(9.29, $this->calculator->execute());
+    }
+
+    public function testCalculateEmpty()
+    {
+        $this->assertEquals(0, $this->calculator->execute());
     }
 }

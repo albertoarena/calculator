ccdc54e6c7f37a73797c31d535c935774510cc11
Alberto Arena <arena.alberto@gmail.com>
2015-02-21 11:41:16 +0000

    Basic structure

---
+++ src/Calculator/Calculator.php
@@ -0,0 +1,27 @@
+<?php
+namespace Calculator;
+
+class Calculator
+{
+
+    public function __construct()
+    {
+
+    }
+
+    public function operator($operator)
+    {
+
+    }
+
+    public function number($number)
+    {
+
+    }
+
+    public function execute()
+    {
+        return 0;
+    }
+
+} 
